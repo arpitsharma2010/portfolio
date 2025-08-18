716c78de3926a58d8c3035d5fe08f5779def31f2
arpitsharma2010 <arpeet.sharma.1998@gmail.com>
2025-08-18 18:27:03 -0400

    resume url updated

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -13,7 +13,7 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ url }) => {
   const [menuOpen, setMenuOpen] = useState(false);
-  const resumePDF = "https://drive.google.com/file/d/1QbMvtAiTB9eITC3ZfUHW6WDwC25Fkb89/view";
+  const resumePDF = "https://drive.google.com/file/d/1IZQt6GwtnYbdodXTXPRmMJrX5xwnaeWo/view";
 
   const navLinks = [
     { to: "/portfolio/", label: "Home" },
